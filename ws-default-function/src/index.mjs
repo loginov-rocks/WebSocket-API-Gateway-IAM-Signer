6dfc3c165b1a74549b55df48d07485b776af3dc0
Danila Loginov <danila@loginov.rocks>
2024-01-13 22:13:18 -0800

    WS Default Function: Respond to the event with any data

--- ws-default-function/src/index.mjs
+++ ws-default-function/src/index.mjs
@@ -1,8 +1,13 @@
+import { ApiGatewayManagementApiClient, PostToConnectionCommand } from '@aws-sdk/client-apigatewaymanagementapi';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
 
 const CONNECTIONS_TABLE_NAME = process.env.CONNECTIONS_TABLE_NAME;
+const WEBSOCKET_API_GATEWAY_ENDPOINT = process.env.WEBSOCKET_API_GATEWAY_ENDPOINT;
 
+const apiGatewayManagementApiClient = new ApiGatewayManagementApiClient({
+  endpoint: WEBSOCKET_API_GATEWAY_ENDPOINT,
+});
 const dynamoDbClient = new DynamoDBClient({});
 const dynamoDbDocumentClient = DynamoDBDocumentClient.from(dynamoDbClient);
 
@@ -11,6 +16,7 @@ export const handler = async (event) => {
 
   const { connectionId } = event.requestContext;
 
+  // Get DynamoDB item saved on $connect.
   const getCommand = new GetCommand({
     Key: { connectionId },
     TableName: CONNECTIONS_TABLE_NAME,
@@ -31,7 +37,27 @@ export const handler = async (event) => {
     return { statusCode: 404 };
   }
 
-  // TODO
+  // Respond to the event with any data.
+  const data = JSON.stringify({
+    dynamoDbItem: getCommandResponse.Item,
+    event,
+  });
+
+  const postToConnectionCommand = new PostToConnectionCommand({
+    Data: data,
+    ConnectionId: connectionId,
+  });
+
+  let postToConnectionCommandResponse;
+  try {
+    postToConnectionCommandResponse = await apiGatewayManagementApiClient.send(postToConnectionCommand);
+  } catch (error) {
+    console.error('error', JSON.stringify(error));
+
+    return { statusCode: 500 };
+  }
+
+  console.log('postToConnectionCommandResponse', JSON.stringify(postToConnectionCommandResponse));
 
   return { statusCode: 204 };
 };
